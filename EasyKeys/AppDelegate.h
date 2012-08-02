1b514a8b86fd9be1fd592e33e13f4f89708ea17d
James Parks <jim.parks3@gmail.com>
2012-08-02 10:41:49 -0500

    Initial Commit

---
+++ EasyKeys/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EasyKeys
+//
+//  Created by iOS Camp on 8/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
